d5ac4e5d9b144df606ddc4c423ed9f4d5f23598d
Tosin Ogunrinde <tosin.ogunrinde@gmail.com>
2021-12-20 13:51:57 +0000

    Refactor to account for activation code form.

--- offerwhere-for-woocommerce/js/offerwhere.js
+++ offerwhere-for-woocommerce/js/offerwhere.js
@@ -1,10 +1,14 @@
 jQuery(document).ready(function ($) {
     'use strict';
     $("#offerwhere-form-ask-user-for-number-toggle-button").click(function () {
-        $('#offerwhere-form-ask-user-for-number').toggle('fast');
+        $('#offerwhere-form-user-number-container').toggle('fast', function () {
+            if ($('#offerwhere-form-user-number-container').is(':visible')) {
+                $('#offerwhere-form-activation-code-container').hide();
+            }
+        });
     });
 
-    if ($('.offerwhere-alert').length) {
-        $('#offerwhere-form-ask-user-for-number').show();
+    if ($('.offerwhere-alert').length && $('#offerwhere-form-activation-code-container').is(':hidden')) {
+        $('#offerwhere-form-user-number-container').show();
     }
 });
